fa43a5cc6148c2f132821e5ff11f053246745f58
Bikash Gogoi <bikashgogoi001@gmail.com>
2016-09-12 22:40:46 +0530

    added kern/syscall

--- kern/trap.c
+++ kern/trap.c
@@ -27,6 +27,7 @@ void int_fpe();
 void int_ali();
 void int_mch();
 void int_sim();
+void int_sys();
 
 static struct Taskstate ts;
 
@@ -87,7 +88,7 @@ trap_init(void)
 	SETGATE(idt[0],  1, GD_KT, &int_div, 0);
 	SETGATE(idt[1],  1, GD_KT, &int_deb, 0);
 	SETGATE(idt[2],  1, GD_KT, &int_nmi, 0);
-	SETGATE(idt[3],  1, GD_KT, &int_brk, 0);
+	SETGATE(idt[3],  1, GD_KT, &int_brk, 3);
 	SETGATE(idt[4],  1, GD_KT, &int_ofl, 0);
 	SETGATE(idt[5],  1, GD_KT, &int_bou, 0);
 	SETGATE(idt[6],  1, GD_KT, &int_ill, 0);
@@ -102,6 +103,7 @@ trap_init(void)
 	SETGATE(idt[17], 1, GD_KT, &int_ali, 0);
 	SETGATE(idt[18], 1, GD_KT, &int_mch, 0);
 	SETGATE(idt[19], 1, GD_KT, &int_sim, 0);
+	SETGATE(idt[48], 1, GD_KT, &int_sys, 3);
 
 	// cprintf("\n%p", &int_div);
 	// cprintf("\n%p", &int_deb);
@@ -179,23 +181,32 @@ print_regs(struct PushRegs *regs)
 	cprintf("  ecx  0x%08x\n", regs->reg_ecx);
 	cprintf("  eax  0x%08x\n", regs->reg_eax);
 }
-
+static int var = 0;
 static void
 trap_dispatch(struct Trapframe *tf)
 {
+	var++;
 	// Handle processor exceptions.
 	// LAB 3: Your code here.
-	if(tf->tf_trapno == 14){
-		page_fault_handler(tf);	
-		return;
+	switch(tf->tf_trapno) {
+		case 3:
+			monitor(tf);
+			return;
+		case 14:
+			page_fault_handler(tf);
+			return;
+		case 48:
+			syscall();
+			return;
 	}
 
 
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
-	if (tf->tf_cs == GD_KT)
+	if (tf->tf_cs == GD_KT) {
 		panic("unhandled trap in kernel");
-	else {
+	} else {
+		cprintf("\nhello %d %p %p\n", var, tf->tf_cs, GD_KT);
 		env_destroy(curenv);
 		return;
 	}
