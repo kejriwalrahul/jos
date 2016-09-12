8da2f95ca12024f42d62835e82674a21ee238462
Bikash Gogoi <bikashgogoi001@gmail.com>
2016-09-13 00:20:30 +0530

    resolved type error in user_mem_check

--- kern/pmap.c
+++ kern/pmap.c
@@ -589,21 +589,21 @@ int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
 	// LAB 3: Your code here.
-	if (va+len >= ULIM) {
+	if ((uint32_t)va+len >= ULIM) {
 		user_mem_check_addr = ULIM;
 		return -E_FAULT;
 	} else {
-		uint32_t btm = ROUNDDOWN(va,PGSIZE);
+		uint32_t btm = ROUNDDOWN((uint32_t)va,PGSIZE);
 		uint32_t tp ;
-		if ((va+len) % PGSIZE == 0) {
-			tp = ROUNDUP(va+len, PGSIZE) + 1;
+		if (((uint32_t)va+len) % PGSIZE == 0) {
+			tp = ROUNDUP((uint32_t)va+len, PGSIZE) + 1;
 		} else {
-			tp = ROUNDUP(va+len, PGSIZE);
+			tp = ROUNDUP((uint32_t)va+len, PGSIZE);
 		}
 		uint32_t i;
-		pte_t *tpage = 1;
-		for (i=va; i<tp; i=i+PGSIZE) {
-			page_lookup(env->env_pgdir,i,&tpage);
+		pte_t *tpage = &i;
+		for (i=(uint32_t)va; i<tp; i=i+PGSIZE) {
+			page_lookup(env->env_pgdir,(void*)i,&tpage);
 			if (!((*tpage) & (perm | PTE_P)) == (perm | PTE_P)) {
 				user_mem_check_addr = i;
 				return -E_FAULT;
